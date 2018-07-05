08940b8605378d5e7a8480b52de41d68c7ed70ca
Abhishek Krishna Shrestha <abhishek.stha17@gmail.com>
2018-07-05 16:21:22 +0545

    corrected the actioncontroller name in index view added migration and updated database

--- TravPense/Data/Migrations/20180705103511_hotelandactivity.Designer.cs
+++ TravPense/Data/Migrations/20180705103511_hotelandactivity.Designer.cs
@@ -11,9 +11,10 @@ using TravPense.Data;
 namespace TravPense.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180705103511_hotelandactivity")]
+    partial class hotelandactivity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TravPense/Data/Migrations/20180705103511_hotelandactivity.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TravPense.Data.Migrations
+{
+    public partial class hotelandactivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateTable(
+                name: "ActivityViewModel",
+                columns: table => new
+                {
+                    Activityid = table.Column<string>(nullable: false),
+                    ActivityName = table.Column<string>(nullable: true),
+                    Duration = table.Column<string>(nullable: true),
+                    LocationType = table.Column<string>(nullable: true),
+                    Price = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityViewModel", x => x.Activityid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelViewModel",
+                columns: table => new
+                {
+                    HotelId = table.Column<string>(nullable: false),
+                    Contact = table.Column<string>(nullable: true),
+                    Destinatino = table.Column<string>(nullable: true),
+                    HotelName = table.Column<string>(nullable: true),
+                    MaxPPP = table.Column<long>(nullable: false),
+                    MinPPP = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelViewModel", x => x.HotelId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "ActivityViewModel");
+
+            migrationBuilder.DropTable(
+                name: "HotelViewModel");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
