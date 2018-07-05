91500b3ccf93df08b7080c09ce10432a9a69a024
Abhishek Krishna Shrestha <abhishek.stha17@gmail.com>
2018-07-05 16:07:07 +0545

    Added Hotel and Activity Models Login and registel html style was changes Unauthorizable fields in ApplicationViews were all hidden added links in Home>Indexview for managin hotels and activities removed Home from navbar in layout(shared view)

---
+++ TravPense/Models/DataBaseViewModels/ActivityViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravPense.Models.DataBaseViewModels
+{
+    public class ActivityViewModel
+    {
+    }
+}
---
+++ TravPense/Models/DataBaseViewModels/HotelViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravPense.Models.DataBaseViewModels
+{
+    public class HotelViewModel
+    {
+        public string HotelId { get; set; }
+        public string HotelName { get; set; }
+        public string Destinatino { get; set; }
+        public string Contact { get; set; }
+        public Int64 MinPPP { get; set; }
+        public Int64 MaxPPP { get; set; }
+    }
+}
